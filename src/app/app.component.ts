c573dda20bfae994ec0c22fbf74fd4e8c010c5a6
Ronit Maity <maity.ronit18@gmail.com>
2024-08-03 00:27:31 +0530

    initial HTML and CSS for the components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EmployeeDetailsComponent } from "./component/employee-details/employee-details.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EmployeeDetailsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
